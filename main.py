39f62223e7674f04edd00ff85df8e16145aea015
Brian Ott <bott@b1-systems.de>
2024-09-03 07:40:48 +0200

    update: main file to import constants and print them to screen

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    print(f"""
+Starting asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}
+    """)
 
 
 if __name__ == "__main__":
