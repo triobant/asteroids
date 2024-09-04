2b3712063fddc651f6d16bb827fcf47f63560430
Brian Ott <bott@b1-systems.de>
2024-09-04 09:41:35 +0200

    update: main function to draw a black screen

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ import pygame
 from constants import *
 
 def main():
-    print(f"""
-Starting asteroids!
-Screen width: {SCREEN_WIDTH}
-Screen height: {SCREEN_HEIGHT}
-    """)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color="black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
