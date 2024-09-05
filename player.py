2a760cb411640f4e40547696a593ef647f5df4eb
Brian Ott <bott@b1-systems.de>
2024-09-05 10:30:46 +0200

    update: Player class to rotate when keys are pressed with the update method

--- player.py
+++ player.py
@@ -21,3 +21,16 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+
+    def rotate(dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
